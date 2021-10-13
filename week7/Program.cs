1def759c3da42a5d45e58976fe90b9706826ea12
Opilane TTHK <opilane@tthk.ee>
2021-10-13 10:48:01 +0300

    7

---
+++ week7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] food = { "võileib juustuga", "jogurt", "banaan", "peekon", "muna" };
+
+            food][3] = "viiner"
+
+            Console.WriteLine("menüüs:");
+
+            for(int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine(food[i]);      
+            }
+        }
+    }
+}
